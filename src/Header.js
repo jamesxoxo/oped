cc4715a583cdd4c4be3565c5b533d71ab2c5d2a9
James Coventry <jamescoventryy@gmail.com>
2018-05-29 17:54:46 +0100

    add in some basic styling as a test

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const NavBar = styled.header`
+  position: sticky;
+  top: 0;
+  right: 0;
+  left: 0;
+  padding: 1rem;
+  background-color: #fff;
+  border-bottom: 1px solid purple;
+`;
+
+const Logo = styled.h1`
+  margin: 0;
+`;
 
 function Header() {
   return (
-    <header>
-      <h1>op-ed</h1>
-    </header>
+    <NavBar>
+      <Logo>op-ed</Logo>
+    </NavBar>
   );
 }
 
