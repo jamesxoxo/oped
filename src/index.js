d793af8231552430b3fdd72157f7fc551df2fcb9
James Coventry <jamescoventryy@gmail.com>
2018-05-30 11:44:31 +0100

    put in some very basic global styles

--- src/index.js
+++ src/index.js
@@ -7,8 +7,68 @@ import registerServiceWorker from './registerServiceWorker';
 
 injectGlobal([
   `
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
   body {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-size: 1rem;
+    line-height: 1.5;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    margin-top 0;
+    margin-bottom: .5rem;
+  }
+
+  p {
+    margin-top: 0;
+    margin-bottom: 1rem;
+  }
+
+  ol,
+  ul,
+  dl {
+    margin-top: 0;
+    margin-bottom: 1rem;
+  }
+
+  ol ol,
+  ul ul,
+  ol ul,
+  ul ol {
+    margin-bottom: 0;
+  }
+
+  dt {
+    font-weight: 700;
+  }
+
+  dd {
+    margin-bottom: .5rem;
+    margin-left: 0;
+  }
+
+  img {
+    vertical-align: middle;
+  }
+
+  label {
+    display: inline-block;
+    margin-bottom: .5rem;
+  }
+
+  input,
+  button,
+  select,
+  optgroup,
+  textarea {
+    font-family: inherit;
+    font-size: inherit;
+    line-height: inherit;
   }
 `,
 ]);
