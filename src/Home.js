8a8db0b9fd307d35a425166b44d821a6a4caf245
James Coventry <jamescoventryy@gmail.com>
2018-05-31 12:12:17 +0100

    update home text

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,15 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 
 import React from 'react';
+import styled from 'styled-components';
 import mp4 from './nonon.mp4';
 import webm from './nonon.webm';
+
+const Video = styled.video`
+  display: block;
+  max-width: 100%;
+  height: auto;
+`;
 
 function Home() {
   return (
@@ -12,11 +19,13 @@ function Home() {
         This website is to help you listen to anime opening/ending songs. Use
         the search box above to get started.
       </p>
+      <p>
+        <Video autoPlay loop muted>
+          <source src={mp4} type="video/mp4" />
+          <source src={webm} type="video/webm" />
+        </Video>
+      </p>
       <p>It relies on the MAL API so if that goes down this goes down.</p>
-      <video autoPlay loop muted>
-        <source src={mp4} type="video/mp4" />
-        <source src={webm} type="video/webm" />
-      </video>
     </article>
   );
 }
