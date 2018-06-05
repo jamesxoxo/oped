3abda579b8498f7cacc0356f65bfac4f6e53acc6
James Coventry <jamescoventryy@gmail.com>
2018-06-05 19:01:44 +0100

    add gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import styled, { ThemeProvider } from 'styled-components';
 import veryUniqueId from './util/veryUniqueId';
 import Header from './Header';
