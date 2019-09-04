daecb8075a08fd520a5a4c54a755a20f04142b48
James Coventry <jamescoventryy@gmail.com>
2019-09-04 17:29:30 +0100

    Updates

---
+++ precache-manifest.09908ace6089ad74ca6080702d91e011.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e1b58e9a6f1924544543c3589e63247",
+    "url": "/op-ed/index.html"
+  },
+  {
+    "revision": "e7a033e69862293f092a",
+    "url": "/op-ed/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e7a033e69862293f092a",
+    "url": "/op-ed/static/js/2.87c6b043.chunk.js"
+  },
+  {
+    "revision": "2bda1a8e2e339500ccae",
+    "url": "/op-ed/static/js/main.2004cb7c.chunk.js"
+  },
+  {
+    "revision": "72389be566ce561d22dc",
+    "url": "/op-ed/static/js/runtime~main.c9aee8b5.js"
+  },
+  {
+    "revision": "03621a224d8c5d8074d98e764a5249db",
+    "url": "/op-ed/static/media/nonon.03621a22.mp4"
+  },
+  {
+    "revision": "13dbfca8857d1792fc14df4b9c132747",
+    "url": "/op-ed/static/media/nonon.13dbfca8.webm"
+  }
+]);
