c65ab4daa9b0b69256f1f875440bad4a67230fba
James Coventry <jamescoventryy@gmail.com>
2019-10-30 14:54:08 +0000

    use https

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,2 +1,2 @@
-export const JIKAN_BASE_URL = '//api.jikan.moe/v3/';
+export const JIKAN_BASE_URL = 'https://api.jikan.moe/v3/';
 export const YOUTUBE_KEY = 'AIzaSyDNS7Q_55JVp_k2lS85S36OIN1WuawqY84';
